ce2b827cc02e0d53f99df32a4cc35ef61273209a
ITSUPPORT <itservizi.helpdesk@gmail.com>
2025-09-04 22:22:03 +0300

    Level 2 update

---
+++ src/ru/javarush/python/core/level03/task11/task11.py
@@ -0,0 +1,12 @@
+# Негатив
+
+# Напишите программу, которая запрашивает у пользователя число и проверяет, является ли оно отрицательным.
+# Используйте оператор if для проверки.
+
+# Напишите тут ваш код
+print("please input an integer")
+x = int(input())
+if x < 0 :
+    print("number is negative")
+else:
+    print("number is positive")
